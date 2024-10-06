52634bbb89e98edd876ecbf60c6db7434df50a17
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-10-06 14:38:07 +0300

    adds the functionality of the todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    excecuteForm(event.target['new-task-description'].value)
+    form.reset();
+  })
 });
+
+function excecuteForm(todoActivities){
+  let p = document.createElement('p');
+  let aButton = document.createElement('button')
+  aButton.addEventListener('click', deleteActivities)
+  aButton.textContent = 'X'
+  p.textContent = `${todoActivities}  `;
+  p.appendChild(aButton)
+  let ul = document.querySelector('#tasks')
+  ul.append(p)
+
+}
+
+function deleteActivities(e) {
+  e.target.parentNode.remove();
+}
